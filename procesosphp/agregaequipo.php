0fcc4c1c598dc93cb94368ad18d8dc33e9b321f5
Victor Segura <seguravictor57@gmail.com>
2022-05-14 16:37:56 -0600

    cambio de redaccion de mensaje al agregar

--- procesosphp/agregaequipo.php
+++ procesosphp/agregaequipo.php
@@ -8,7 +8,7 @@ $pais = $_POST['PAIS'];
 $query = "INSERT INTO EQUIPOS(NOMBRE, PAIS) VALUES ('$nombre', '$pais')";
 if ($mysqli->query($query)) {
 
-    $_SESSION['mensaje1'] = "Se ha agregardo el usuario: " . $nombre;
+    $_SESSION['mensaje1'] = "Se ha agregardo el equipo: " . $nombre;
 
     header('Location: http://34.207.191.253/sistema_colegio_humanista/menu_admin.php');
 } else {
