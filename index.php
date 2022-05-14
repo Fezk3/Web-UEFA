d48e9a0501d3276032345b873570c044e6d03b06
Jefté Andrés Vega Hidalgo <jefte.vega.hidalgo@est.una.ac.cr>
2022-05-13 20:47:06 -0600

    login

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -15,6 +18,18 @@
   <header>
     <?php include_once("navbar.php"); ?>
   </header>
+
+  <?php
+  if (isset($_SESSION['mensajeBienvenida'])) {
+      ?>
+      <div class="alert alert-primary alert-dismissible fade show mt-1" role="alert">
+          <strong><?php echo $_SESSION['mensajeBienvenida'] ?></strong>
+          <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+      </div>
+      <?php
+      unset($_SESSION['mensajeBienvenida']);
+  }
+  ?>
 
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
---
+++ procesosphp/checklogin.php
@@ -0,0 +1,40 @@
+<?php
+session_start();
+
+if (empty($_POST)) {
+    header('Location: http://localhost:63342/Web-UEFA/index.php');
+    exit();
+}
+
+require_once './conexion.php';
+
+$userID = $_POST['cedula'];
+$password = md5($_POST['password']);
+
+$sql = "SELECT * FROM administradores WHERE CEDULA = '$userID'";
+
+$result = $mysqli->query($sql);
+
+if ($result->num_rows <= 0) {
+    echo "Username o Password incorrectos. <br>";
+    echo "<a href='../login.php'>VOLVER A INTENTARLO</a> ";
+    exit();
+}
+$row = $result->fetch_array(MYSQLI_ASSOC);
+
+if ($password == $row['PASSWORD']) {
+
+    $_SESSION['loggedin'] = true;
+    $_SESSION['username'] = $userID;
+    $_SESSION['start'] = time();
+    $_SESSION['expire'] = $_SESSION['start'] + (60 * 60);
+
+    $_SESSION['mensajeBienvenida'] = "Bienvenido de nuevo " . $userID;;
+
+    header('Location: http://localhost:63342/Web-UEFA/index.php');
+    exit();
+} else {
+    echo "Username o Password incorrectos. <br>";
+    echo "<a href='../login.php'>VOLVER A INTENTARLO</a> ";
+}
+mysqli_close($conexion);
