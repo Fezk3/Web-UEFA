7cb8496574a43f4cda9337c48ecb1d3ba7d5ce1d
Victor Segura <seguravictor57@gmail.com>
2022-05-14 14:36:14 -0600

    prototipo pag admin

---
+++ admin.php
@@ -0,0 +1,139 @@
+<?php
+session_start();
+
+if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
+} else {
+    echo "Esta página es solo para usuarios registrados. <br> <br>";
+    echo "<a href='./index.php'>LOGIN</a>";
+    exit();
+}
+
+$now = time();
+
+if ($now > $_SESSION['expire']) {
+    session_destroy();
+
+    echo "Su sesión ha caducado";
+    echo "<a href='./index.php'>NECESITA HACER LOGIN</a>";
+    exit();
+}
+
+if ($_SESSION['tipo_usuario'] != 1) {
+    //   header('Location: http://34.207.191.253/sistema_colegio_humanista/index.php');
+    //    exit();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Administracion UEFA</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
+    <link rel="stylesheet" href="styleadmin.css">
+</head>
+
+<body>
+
+    <header>
+        <?php include_once("navbar.php"); ?>
+    </header>
+
+    <di class="container">
+        <div class="row">
+            <div class="col">
+                <h1 class="text-center">Administracion de partidos</h1>
+            </div>
+        </div>
+    </di>
+
+    <div class="container">
+        <div class="row">
+
+            <form action="./procesosphp/agregaruser.php" class="needs-validation" method="POST" novalidate>
+
+                <div class="col-12 col-md-8 m-auto">
+                    <div class="mb-3">
+                        <label for="exampleFormControlInput1" class="form-label ">Nombre del Equipo</label>
+                        <input required class="form-control" name="equipo" id="exampleFormControlInput1" />
+                        <div class="invalid-feedback">
+                            Ingrese un Nombre de equipo
+                        </div>
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="exampleFormControlInput1" class="form-label ">Pais del equipo</label>
+                        <input required class="form-control" id="exampleFormControlInput1" name="pais" />
+                        <div class="invalid-feedback">
+                            Ingrese un Pais
+                        </div>
+                    </div>
+
+                </div>
+        </div>
+    </div>
+
+    <div class="row justify-content-center align-items-center">
+        <div class="col-5 col-md-3 text-center">
+            <button class="btn btn-success btn-block btn-lg mt-4" style="width: 100%;">Agregar</button>
+        </div>
+    </div>
+
+    </form>
+
+    <!-- tabla con los equipos -->
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col">
+                <h1 class="text-center">Equipos Registrados</h1>
+            </div>
+        </div>
+
+        <div class="container mt-4">
+            <div class="row d-flex justify-content-center align-items-center">
+                <div class="col-md-10 ">
+                    <table class="table table-success table-hover table-bordered">
+                        <thead>
+                            <tr>
+                                <th scope="col">Equipo</th>
+                                <th scope="col">Pais</th>
+                            </tr>
+                        </thead>
+                        <tbody id="info">
+                            <?php
+                            require_once "procesosphp/conexion.php";
+                            $query = "SELECT CEDULA, NOMBRE FROM ADMINISTRADORES WHERE TIPO = 2";
+                            $consulta = $mysqli->query($query);
+                            if ($consulta->num_rows >= 1) {
+                                while ($fila = $consulta->fetch_array(MYSQLI_ASSOC)) {
+                                    echo "<tr>
+                                <td class='text-center op '>" . $fila['CEDULA'] . "</td>
+                                <td class='text-center op '>" . $fila['NOMBRE'] . "</td>
+                               <td class='text-center op'>
+                                    <a class='btn btn-danger editar' href=procesosphp/eliminauser.php?CEDULA=" . $fila['CEDULA'] . ">Eliminar</a>
+                                </td>
+                              </tr>";
+                                }
+                                echo "</tbody>
+                        </table>";
+                            } else {
+                                echo "<h1 text-center>Error en la base de datos</h1>";
+                            }
+                            ?>
+                </div>
+            </div>
+        </div>
+
+
+    </div>
+    </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
