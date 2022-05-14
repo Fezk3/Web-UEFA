a757b5d7e47213b54618a2819a377cf14e14dd95
Jefté Andrés Vega Hidalgo <jefte.vega.hidalgo@est.una.ac.cr>
2022-05-14 14:48:25 -0600

    Tabla posiciones Carrusel

--- index.php
+++ index.php
@@ -30,6 +30,11 @@ session_start();
       unset($_SESSION['mensajeBienvenida']);
   }
   ?>
+  
+  <main>
+      <h6><a style="text-decoration: none" href="tablaPosiciones/posiciones.php">TABLA POSICIONES </a></h6>
+
+  </main>
 
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
---
+++ tablaPosiciones/tabla.php
@@ -0,0 +1,34 @@
+<div class="container mt-4">
+    <div class="row d-flex justify-content-center align-items-center">
+        <div class="col-md-10 ">
+            <table class="table table-success table-hover table-bordered">
+                <thead>
+                <tr>
+                    <th scope="col">CLUB</th>
+                    <th scope="col">PJ</th>
+                    <th scope="col">G</th>
+                    <th scope="col">E</th>
+                    <th scope="col">P</th>
+                    <th scope="col">GF</th>
+                    <th scope="col">GC</th>
+                    <th scope="col">DG</th>
+                    <th scope="col">PTS</th>
+                </tr>
+                </thead>
+                <tbody id="info">
+                <tr>
+                    <td>A</td>
+                    <td>B</td>
+                    <td>C</td>
+                    <td>D</td>
+                    <td>E</td>
+                    <td>F</td>
+                    <td>G</td>
+                    <td>H</td>
+                    <td>I</td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
