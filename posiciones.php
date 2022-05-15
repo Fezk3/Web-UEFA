864b55bbdad9875bd0fae2dd777bad9bbbefd85c
Jefté Andrés Vega Hidalgo <jefte.vega.hidalgo@est.una.ac.cr>
2022-05-15 16:38:20 -0600

    Eliminar punto en src
    Se eliminó el doble punto en el src de css y de js

--- posiciones.php
+++ posiciones.php
@@ -21,8 +21,8 @@ while ($row = $resultado->fetch_assoc()) {
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"/>
-    <link rel="stylesheet" href="../css/style.css"/>
-    <script src="../js/script.js" defer></script>
+    <link rel="stylesheet" href="./css/style.css"/>
+    <script src="./js/script.js" defer></script>
 </head>
 <body>
 
