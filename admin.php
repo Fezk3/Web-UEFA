e749ac2747a8d201c4d399af85839f69ec66ced1
Victor Segura <seguravictor57@gmail.com>
2022-05-15 19:30:53 -0600

    valida form con require en admin

--- admin.php
+++ admin.php
@@ -192,29 +192,7 @@ if ($_SESSION['tipo_usuario'] != 1) {
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
-    <script>
-        // Example starter JavaScript for disabling form submissions if there are invalid fields
-        (function() {
-            'use strict'
-
-            // Fetch all the forms we want to apply custom Bootstrap validation styles to
-            var forms = document.querySelectorAll('.needs-validation')
-
-            // Loop over them and prevent submission
-            Array.prototype.slice.call(forms)
-                .forEach(function(form) {
-                    form.addEventListener('submit', function(event) {
-                        if (!form.checkValidity()) {
-                            event.preventDefault()
-                            event.stopPropagation()
-                        }
-                        form.classList.add('was-validated')
-                    }, false)
-                })
-        })()
-    </script>
-
-    <script src="./js/app.js"></script>
+    <?php require_once('./procesosphp/validaform.php') ?>
 
 </body>
 
